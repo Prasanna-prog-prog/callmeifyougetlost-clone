cca681c4b3189d05fc251a8352de662c33fc53fd
prasannanjali <prasannanjalipuchakayala@gmail.com>
2026-06-14 17:41:46 +0530

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,17 +1,41 @@
-const Personname = "PPP";
-let age = 20;
-const city = "XYZ ON EARTH";
-const  hobbies = "PAINTING";
+let nameInput = document.getElementById("nameInput");
+let cardName = document.getElementById("cardName");
+
+nameInput.addEventListener("input" , function(){
+ cardName.textContent = nameInput.value;
+});
+
+let birth = document.getElementById("birth");
+let cardbirth = document.getElementById("cardbirth");
+
+birth.addEventListener("input" , function(){
+ cardbirth.textContent = birth.value;
+});
+
+let loc = document.getElementById("loc");
+let cardloc= document.getElementById("cardloc");
+
+loc.addEventListener("input" , function(){
+ cardloc.textContent = loc.value;
+});
 
 
+// When button clicked → trigger file input click
+let fileInput = document.getElementById("fileInput");
 
-document.getElementById("Personname").innerText = Personname;
-document.getElementById("age").innerText = age;
-document.getElementById("city").innerText = city;
-document.getElementById("hobbies").innerText = hobbies;
+document.getElementById("uploadBtn").addEventListener("click" , function(){
+ console.log("button clicked!"); // add this
+    fileInput.click();
+});
 
+fileInput.addEventListener("change" , function(){
+const file = this.files[0];
+const reader = new FileReader();
 
-function increaseAge(){
-    age = age + 1;
-    document.getElementById("age").innerText = age;
+reader.onload = function(e){
+    document.getElementById("cardPhoto").src = e.target.result;
+    document.getElementById("cardPhoto").style.display = "block";
+    document.getElementById("photoPlaceholder").style.display = "none";
 }
+reader.readAsDataURL(file);
+});
